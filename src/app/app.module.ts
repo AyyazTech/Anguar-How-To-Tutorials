256d76ea836711d4a42efd4be5686e883322b5d0
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2023-08-03 14:54:37 +0500

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AccordionComponent } from './accordion/accordion.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, AccordionComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
