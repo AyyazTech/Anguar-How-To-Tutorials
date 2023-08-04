bc5d4078a4cb0f864d812dcae480de544e8c427e
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2023-08-04 16:03:51 +0500

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PaginationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
