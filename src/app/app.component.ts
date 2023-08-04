6c3909b9424df16d9c7ff64d48daca1a8823f1bc
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2023-08-04 17:20:01 +0500

    disableButtonOnClick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  imageUrl = 'assets/images/example.jpg';
-  imageWidth = 400;
-  isResponsive = false;
+  processing = false;
+  showSuccessMessage = false;
+  form: FormGroup = this.fb.group({
+    email: [''],
+    password: [''],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  submit() {
+    this.showSuccessMessage = false;
+    this.processing = true;
+    console.log(this.form.value);
+    setTimeout(() => {
+      this.form.reset();
+      this.processing = false;
+      this.showSuccessMessage = true;
+
+      setTimeout(() => {
+        this.showSuccessMessage = false;
+      }, 2000);
+    }, 4000);
+  }
 }
